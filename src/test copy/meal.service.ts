73465af8863770e92fda074208d2f610ad2ca9a1
Nguyễn Quốc Trọn <90500313+QuocTron@users.noreply.github.com>
2022-12-14 13:15:05 +0700

    deploy

--- src/test copy/meal.service.ts
+++
@@ -1,7 +0,0 @@
-import { CreateMealInput } from "../schema/meal.schema";
-import MealModel, { MealDocument } from "./../model/meal.model";
-
-export const createMeal = async (mealInput: CreateMealInput) => {
-  console.log(mealInput);
-  return MealModel.create(mealInput);
-};
